694191cb94ed662b324a291574148f11708cd8c5
gavrilmikhailov <gavrilmikhailov@gmail.com>
2021-02-27 15:09:31 +0900

    Updated AnimationComponent

--- PlagueOrigins/PlagueOrigins/AnimationComponent.cpp
+++ PlagueOrigins/PlagueOrigins/AnimationComponent.cpp
@@ -28,47 +28,35 @@ void AnimationComponent::initArmature(sf::Vector2f vec)
 
 dragonBones::SFMLArmatureDisplay* AnimationComponent::playAnimation(int key, float posX, float posY)
 {
+	this->armatureDisplay->getArmature()->dispose();
 	switch (key)
 	{
 	case IDLE:
 		this->armatureDisplay = new dragonBones::SFMLArmatureDisplay("ArmatureheroIdle");
-
-
 		this->armatureDisplay->getAnimation()->play("Idle");
 		break;
 	case RUN_UP:
 		this->armatureDisplay = new dragonBones::SFMLArmatureDisplay("ArmatureheroRunN");
-
-
 		this->armatureDisplay->getAnimation()->play("RunN");
 		break;
 	case RUN_RIGHT:
 		this->armatureDisplay = new dragonBones::SFMLArmatureDisplay("ArmatureheroRunEW");
-
-
 		this->armatureDisplay->getAnimation()->play("RunEW");
 		break;
 	case RUN_DOWN:
 		this->armatureDisplay = new dragonBones::SFMLArmatureDisplay("ArmatureheroRunS");
-
-
 		this->armatureDisplay->getAnimation()->play("RunS");
 		break;
 	case RUN_LEFT:
 		this->armatureDisplay = new dragonBones::SFMLArmatureDisplay("ArmatureheroRunEW");
-
 		this->armatureDisplay->getArmature()->setFlipX(true);
 		this->armatureDisplay->getAnimation()->play("RunEW");
 		break;
 	case ROLL:
 		/*
-
 			WIP
-
 		*/
 		break;
 	}
 	return armatureDisplay;
 }
-
-
