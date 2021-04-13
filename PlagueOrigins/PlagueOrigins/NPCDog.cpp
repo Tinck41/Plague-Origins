7ac44a430ba361c241d35689ecd1677ec7368e67
uufswe21 <43157713+uufswe21@users.noreply.github.com>
2021-04-13 12:52:15 +0900

    update factory fix

--- PlagueOrigins/PlagueOrigins/NPCDog.cpp
+++ PlagueOrigins/PlagueOrigins/NPCDog.cpp
@@ -82,7 +82,7 @@ void NPCDog::update(const float& dt)
 
 	//animation
 	animationComponent->getArmatureDisplay()->setPosition(sf::Vector2f((1 / scale) * (shape.getPosition().x + colliderComponent->getHalfSize().x), (1 / scale) * (shape.getPosition().y + colliderComponent->getHalfSize().y)));
-	animationComponent->updateFactory(dt);
+	//animationComponent->updateFactory(dt);
 
 }
 
