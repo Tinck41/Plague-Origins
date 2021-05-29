f62285f52008b0ce139be681c967bf6360892f0f
Tinck <glushkov2000@bk.ru>
2021-05-29 13:42:57 +0900

    PO-83 boss fight music

--- PlagueOrigins/PlagueOrigins/Src/ECS/Components/AmbienceAudioSource.h
+++ PlagueOrigins/PlagueOrigins/Src/ECS/Components/AmbienceAudioSource.h
@@ -4,9 +4,13 @@ struct AmbienceAudioSource
 {
 	sf::Sound windSound;
 	sf::Sound torchSound;
+	sf::Sound bossFightSound;
 
 	bool playWindSound = true;
 	bool loopWindSound = true;
+
+	bool playBossFightSound = false;
+	bool loopBossFightSound = false;
 
 	bool playTorchSound = true;
 	bool loopTorchSound = true;
